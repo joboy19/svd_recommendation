86df44becbac3bd60a5cee236986bc4ff32a7043
joboy19 <joeboyd9111@gmail.com>
2019-11-22 10:53:12 +0000

    testing

--- test.py
+++ test.py
@@ -4,8 +4,10 @@ import random as random
 import csv
 from scipy.sparse.linalg import svds
 
+k = 2
 
-ratings = pd.read_csv("ratings.csv")
+
+ratings = pd.read_csv("ratings copy 2.csv")
 books = pd.read_csv("clean_test.csv")
 #print(ratings.head())
 
@@ -14,8 +16,33 @@ r = rdf.values
 mean = np.mean(r, axis=1)
 demean = r - mean.reshape(-1, 1)
 
-U, sigma, Vt = svds(demean, k=50)
+print(r)
+
+U, sigma, Vt = svds(demean, k=k)
 sigma = np.diag(sigma)
+
+#Vt = np.matmul(sigma, Vt)
+
+vals = np.matmul(U, Vt) + mean.reshape(-1, 1)
+
+#to get a prediction for a pre-existing user: get the values at vals[userID, book_ID]
+#to get best predictions for a pre-existing user: find max of unseen values
+
+newP = np.array([5,0,0,0,5])
+
+print(newP)
+print(U)
+print(Vt)
+
+newV = np.matmul(newP, Vt)
+
+
+print(newV)
+
+
+
+
+
 
 
 
