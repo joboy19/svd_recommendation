77a89ca552dcd4205fadd223a1dbc23d6d2854ab
joboy19 <joeboyd9111@gmail.com>
2019-12-13 03:55:52 +0000

    final

--- static/script.js
+++ static/script.js
@@ -20,22 +20,146 @@ function update_ratings(){
     
 }
 
+function get_recommendations(){
+    $.ajax({
+        url: "/get_recoms",
+        method:"GET",
+        success: function(data) {
+            if (data != "False"){
+                var vals = JSON.parse(data);
+                console.log(vals);
+                $("#table2 tbody > tr").remove();
+                for (var key in vals){
+                    $('#table2 tbody').append('<tr><td>' + key + '</td><td>' + 
+                        vals[key][0] + 
+                        '</td><td>' + vals[key][1] + '</td></tr>');
+                }
+            } 
+        }
+    });
+}
+
 function login(username) {
     $("#login").toggle();
     $("#main").toggle();
+    $("#nav").toggle();
     $("#username_main").val(username);
     update_ratings();
+    get_recommendations();
 
 }
 
+function logout(){
+    $.ajax({
+        url: "/logout",
+        method:"POST",
+        success: function(data) {
+            if (data != "False"){
+                logout_change();
+            }
+        }
+        })
+}
+
+function logout_change(){
+    $("#login").toggle();
+    $("#main").toggle();
+    $("#nav").toggle();
+}
+
+function toggle_nation(){
+    console.log($("#nation").html());
+    var nation = $("#nation").html();
+    if (nation == "Spanish"){
+        update_language(1);
+    } else {
+        update_language(2);
+    }
+}
+
+function update_label(value){
+    if (value == "False"){
+        if ($("#nation").html() == "Spanish"){
+            $("#outLabel").html("El libro no fue encontrado.");
+        } else {
+            
+            $("#outLabel").html("The book was not found.");
+        };
+    } else {
+        if ($("#nation").html() == "Spanish"){
+            
+            $("#outLabel").html("Ese libro tiene ID: " + value);
+        } else {
+            $("#outLabel").html("That book has ID: " + value);
+        };
+    }
+}
+
+function update_language(val){
+    if (val == 1){
+        var labels = trans_vals["en"];
+        $("#nation").html("English")
+    } else { 
+        var labels = trans_vals["sp"];
+        $("#nation").html("Spanish")
+    }
+
+    $("#signin-text").html(labels[0]);
+    $("#signup").html(labels[1]);
+    $("#signin-button").html(labels[2]);
+    $("#button-logout").html(labels[3]);
+    $("#username-label").html(labels[4]);
+    $("#username-details").html(labels[5]);
+    $("#rating-label").html(labels[6]);
+    $("#remove-tag").html(labels[7]);
+    $("#confirm-button").html(labels[8]);
+    $("#label-addbook").html(labels[9]);
+    $("#confirm-button2").html(labels[10]);
+    $("#label-main").html(labels[11]);
+    $("#tag-book").html(labels[12]);
+    $("#tag-name").html(labels[13]);
+    $("#tag-genre").html(labels[14]);
+    $("#label-recom").html(labels[15]);
+    $("#tag-book2").html(labels[16]);
+    $("#tag-name2").html(labels[17]);
+    $("#tag-genre2").html(labels[18]);
+    $("#name").attr("placeholder", labels[19]);
+    $("#genre").attr("placeholder", labels[20]);
+    $("#bookid").attr("placeholder", labels[21]);
+    $("#username").attr("placeholder", labels[22]);
+    $("#password").attr("placeholder", labels[23]);
+    $("#tag-rating").html(labels[24]);
+    $("#label-searchbook").html(labels[25])
+    $("#searchname").attr("placeholder", labels[26])
+    $("#search-button").html(labels[27])
+}
+
+
+
 $( document ).ready(function(event){
+    $("#button").click( function() {
+        logout();
+    });
+    $("#nation").click( function() {
+        toggle_nation();
+    });
+    $("#button-logout").click( function() {
+        $.ajax({
+            url: "/login",
+            method:"post",
+            success: function(data){
+                location.reload()
+            }
+        });
+    });
+
     $("#form_login").submit( function(event){
         event.preventDefault();
         var signup_val = 0;
         if ($('#signup').is(":checked")){
             signup_val = 1;
         };
-        console.log(signup_val)
+        console.log(signup_val);
         $.ajax({
             url: "/login",
             data: {
@@ -71,6 +195,20 @@ $( document ).ready(function(event){
           });
     });
 
+    $("#form-search").submit( function(event){
+        event.preventDefault();
+        $.ajax({
+            url: "/search_books",
+            data: {
+                bookname: $("#searchname").val()
+            },
+            method:"get",
+            success: function(data) {
+                update_label(data);
+            }
+          });
+    });
+
     $("#form_rating").submit( function(event){
         event.preventDefault();
         console.log($("#bookid").val());
@@ -87,4 +225,22 @@ $( document ).ready(function(event){
             }
           });
     });
+
+    $("#form_book").submit( function(event){
+        event.preventDefault();
+        console.log($("#name").val());
+        console.log($("#genre").val());
+        $.ajax({
+            url: "/add_book",
+            data: {
+                bookname: $("#name").val(),
+                genre: $("#genre").val()
+            },
+            method:"post",
+            success: function(data) {
+                window.alert("Book has been added. ID:"+data)
+            }
+          });
+    });
+    toggle_nation();
 })
--- web_tech.py
+++ web_tech.py
@@ -3,6 +3,7 @@ import pandas as pd
 import json
 import csv
 
+from recommendations import make_prediction
 from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
@@ -21,7 +22,7 @@ def getID(username):
 @app.route("/get_ratings", methods=["GET"])
 def get_ratings():
     out = {}
-    with open("data/ratings copy 2.csv", "r", encoding='utf-8', newline='\n') as data1:
+    with open("data/ratings.csv", "r", encoding='utf-8', newline='\n') as data1:
         for line in data1:
             data = line.rstrip().split(",")
             if data[0] == session["id"]:
@@ -31,11 +32,10 @@ def get_ratings():
     return "False"
 
 def get_book(id_):
-    with open("data/books_test.csv", "r", encoding='utf-8', newline='\n') as data1:
+    with open("data/books.csv", "r", encoding='utf-8', newline='\n') as data1:
         for line in data1:
             data = line.rstrip().replace(", ", " ").replace("\"", "").split(",")
             if data[0] == id_:
-                print(data)
                 return data[1], data[2]
     return "False" 
 
@@ -53,18 +53,16 @@ def checkuser(username, password):
     return False
 
 def signup(username, password):
-    count = 0
     print("Signup For: ", username)
     with open("data/users.csv", "r", encoding='utf-8', newline='\n') as data1:
         for line in data1:
-            count += 1
             data = line.rstrip().split(",")
-            print(data)
+            last_id = data[0]
             if data[1] == username:
                 return "False"
     with open("data/users.csv", "a", encoding='utf-8', newline="\n") as data1:
         writer = csv.writer(data1)
-        writer.writerow([count+1,username,password])
+        writer.writerow([int(last_id)+1,username,password])
         data1.close()
     return True
         
@@ -78,7 +76,6 @@ def login():
     val = checkuser(request.args["username"], request.args["password"])
     if val:
         session["id"] = getID(request.args["username"])
-        print(session["id"], "here")
         return "True"
     return "False"
 
@@ -86,7 +83,6 @@ def login():
 def update_details():
     r = csv.reader(open("data/users.csv"))
     data = list(r)
-    print(data)
     for x in range(len(data)):
         if data[x][0] == session["id"]:
             print("here")
@@ -98,39 +94,73 @@ def update_details():
     return "False"
 
 def delete_rating(bookid):
-    r = csv.reader(open("data/ratings copy 2.csv"))
+    r = csv.reader(open("data/ratings.csv"))
     data = list(r)
-    print(data)
     for x in range(len(data)):
         if data[x][0] == session["id"]:
             if data[x][1] == bookid:
                 data.pop(x)
-                writer = csv.writer(open('data/ratings copy 2.csv', 'w', newline='\n', encoding='utf-8'))
+                writer = csv.writer(open('data/ratings.csv', 'w', newline='\n', encoding='utf-8'))
                 writer.writerows(data)
                 return "True"
 
 @app.route("/make_rating", methods=["POST"])
 def make_rating():
-    print(request.form)
     if request.form["rating"] == "-1":
         delete_rating(request.form["bookid"])
         return "True"
     else:
-        r = csv.reader(open("data/ratings copy 2.csv"))
+        r = csv.reader(open("data/ratings.csv"))
         data = list(r)
         for x in range(len(data)):
             if data[x][0] == session["id"]:
                 if data[x][1] == request.form["bookid"]:
                     print("here")
                     data[x] = [data[x][0], data[x][1], request.form["rating"]]
-                    writer = csv.writer(open('data/ratings copy 2.csv', 'w', newline='\n', encoding='utf-8'))
+                    writer = csv.writer(open('data/ratings.csv', 'w', newline='\n', encoding='utf-8'))
                     writer.writerows(data)
                     return "True"
         data.append([session["id"], request.form["bookid"], request.form["rating"]])
-        writer = csv.writer(open('data/ratings copy 2.csv', 'w', newline='\n', encoding='utf-8'))
+        writer = csv.writer(open('data/ratings.csv', 'w', newline='\n', encoding='utf-8'))
         writer.writerows(data)
         return "True"
-    
+
+@app.route("/add_book", methods=["POST"])
+def add_book():
+    r = csv.reader(open('data/books.csv', encoding="utf-8"))
+    data = list(r)
+    book_id = len(data)
+    print(book_id)
+    data.append([book_id, request.form["bookname"], request.form["genre"]])
+    writer = csv.writer(open('data/books.csv', 'w', newline='\n', encoding='utf-8'))
+    writer.writerows(data)
+    return str(book_id)
+
+@app.route("/get_recoms", methods=["GET"])
+def get_recoms():
+    out = {}
+    bookIDs = make_prediction(str(session["id"]))
+    for x in bookIDs:
+        a = get_book(str(x))
+        out[x] = [a[0], a[1]]
+    print(out)
+    return json.dumps(out)
+
+@app.route("/search_books", methods=["GET"])
+def search_books():
+    book = request.args["bookname"]
+    with open("data/books.csv", "r", encoding='utf-8', newline='\n') as data1:
+        for line in data1:
+            data = line.rstrip().split(",")
+            if data[1] == book:
+                return str(data[0])
+    return "False"
+
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    session.clear()
+    return "True"
     
 
 if __name__ == '__main__':
